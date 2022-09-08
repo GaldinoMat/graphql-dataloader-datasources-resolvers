84629d9888dcafcb46fdad7d6a68a0af61e5c093
Galdino <mateusgaldino1000@hotmail.com>
2022-09-08 16:59:10 -0300

    refactor: :recycle: exclude specific typedefs for new session focused on resolvers

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,18 +1,8 @@
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts(id);
-
   const post = await response.json();
 
-  if (Math.random() < 0.3)
-    return {
-      statusCode: 500,
-      message: 'Timeout error',
-      timeout: 123,
-    };
-
-  return typeof post.id === 'undefined'
-    ? { statusCode: 404, message: 'Post not found', postId: id }
-    : post;
+  return post;
 };
 
 const posts = async (_, { inputFilters }, { getPosts }) => {
@@ -26,22 +16,5 @@ export const postResolvers = {
   Query: {
     post,
     posts,
-  },
-  PostResult: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      if (typeof obj.id !== 'undefined') return 'Post';
-
-      return null;
-    },
-  },
-  PostError: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-
-      return null;
-    },
   },
 };
